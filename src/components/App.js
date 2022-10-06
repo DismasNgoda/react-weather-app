239b34373a2a02a57e037205a6e2f60ab26750a1
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-10-06 08:38:35 +0300

    sets the display for temp <20

--- src/components/App.js
+++ src/components/App.js
@@ -48,14 +48,21 @@ function App() {
         {(typeof weather.main != "undefined") ? (
         <div>
           <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{myDate(new Date())}</div>
+            <div className="location">
+              {weather.name}
+              {weather.sys.country}
+            </div>
+            <div className="date">
+              {myDate(new Date())}
+            </div>
           </div>
           <div className="weather-box">
             <div className="temp">
               {Math.round(weather.main.temp)}°c
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
+            <div className="weather">
+              {weather.weather[0].main}
+              </div>
           </div>
         </div>
         ) : ('')}
