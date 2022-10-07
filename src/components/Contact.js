bb6da6c29c9b3ec61992ae2ca95c61cd0679328b
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-10-07 22:15:01 +0300

    updates contact page and db.json

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,54 +1,51 @@
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import '../App.css'
 
 function Contact() {
-    
-    const [firstName, setFirstName] = useState(null);
-    const [lastName, setLastName] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [textarea,setTextArea] = useState(null);
-
-  const handleInputChange = (e) => {
-        const {id , value} = e.target;
-        if(id === "firstName"){
-            setFirstName(value);
-        }
-        if(id === "lastName"){
-            setLastName(value);
-        }
-        if(id === "email"){
-            setEmail(value);
-        }
-        if(id === "textarea"){
-          setTextArea(value);
-        }
-
-    }
-
-    const handleSubmit  = () => {
-        console.log(firstName,lastName,email,textarea);
-    }
-<Link to="/"> Contact</Link>
+    const [userData, setUserData] = useState([]);
+    const [formInputData, setFormInputData] = useState({
+		email: "",
+		message: ""
+	});
+    const addRecord = (data) => {
+		userData([...userData, data]);
+	};
+	const subscribe = async (event) => {
+		event.preventDefault();
+		try {
+			const fetchData = await fetch("http://localhost:4000/users", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					email: formInputData.email,
+					message: formInputData.message
+				}),
+			});
+			const fetchResponse = await fetchData.json();
+			addRecord(fetchResponse);
+			setFormInputData({
+				email: "",
+				message: "",
+			});
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	const captureUserEntries = (e) => {
+		setFormInputData({ ...formInputData, [e.target.email]: e.target.value });
+	};
     return(
       // <Link to="/"> Contact</Link>
         <div className="form">
             <div className="form-body">
-                <div className="username">
-                    <input className="form__input" type="text" value={firstName} onChange = {(e) => handleInputChange(e)} id="firstName" placeholder="First Name"/>
-                </div>
-                <div className="lastname">
-                    <input  type="text" name="" id="lastName" value={lastName}  className="form__input" onChange = {(e) => handleInputChange(e)} placeholder="LastName"/>
-                </div>
-                <div className="email">
-                    <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
-                </div>
-                <div className="textarea">
-                    <input className="form__input" type="textarea"  id="textarea" value={textarea} onChange = {(e) => handleInputChange(e)} placeholder="Enter Message ..."/>
-                </div>
+                <input  type="email" name="email" id="email" value={formInputData.email} onChange={captureUserEntries} placeholder="Email"/>
+                <input  type="textarea" name='message' id="textarea" value={formInputData.message} onChange={captureUserEntries} placeholder="Enter Message ..."/>
             </div>
             <div class="footer">
-                <button onClick={()=>handleSubmit()} type="submit" class="btn">Submit</button>
+                <button onSubmit={subscribe} type="submit" class="btn">Submit</button>
             </div>
         </div>
        
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import '../App.css';
-// import {
-//     Nav,
-//     NavLink,
-//     Bars,
-//     NavMenu,
-//     NavBtn,
-//     NavBtnLink,
-//   } from './NavbarElements';
+
 
 function NavBar() {
   return (
     <div className='nav-bar'>
         <ul className='nav-bar-elements'>
-        <Link to='/contact'><li>Contact Us</li></Link>
-        <Link to='/about'> <li>About</li></Link>
-        <Link to='/blog'><li>Blog</li></Link>
+        <Link to='/contact'>Contact Us</Link>
+        <Link to='/about'> About</Link>
+        <Link to='/blog'>Blog</Link>
         </ul>
     </div>
   )
