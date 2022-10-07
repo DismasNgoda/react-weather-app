8265c41ba6f5ad91798f65edb1d7d85b1e622d41
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-10-07 07:18:41 +0300

    adds the navbar links

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import Search from './Search';
 import Contact from './Contact';
 import {Routes, Route} from 'react-router-dom';
 import NavBar from './NavBar';
+import About from './About';
+import Blog from './Blog';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Search />
       <Routes>
         <Route path="/contact" element={ <Contact/> } />
+        <Route path="/about" element={ <About/> } />
+        <Route path="/blog" element={ <Blog/> } />
       </Routes>
       
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function NavBar() {
   return (
     <div>
         <ul>
             <li>
-                <a href='/contact'>Contact Us</a>
+                <Link to='/contact'>Contact Us</Link>
+            </li>
+            <li>
+                <Link to='/contact'>About</Link>
+            </li>
+            <li>
+                <Link to='/contact'>Blog</Link>
             </li>
         </ul>
     </div>
